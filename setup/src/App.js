8035c25d4ef6be869ce0f27022d3d2161a567b12
MaianneThornton <maianne.thornton@gmail.com>
2021-11-22 14:24:03 -0500

    Created and added functionality to refresh button. Initial version of project is complete

--- setup/src/App.js
+++ setup/src/App.js
@@ -45,6 +45,19 @@ function App() {
       </main>
     );
   }
+  // if there are no tours in the state value then return no tours left and give the option to fetch data once again
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={fetchTours}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
   // if loading is false (data has been fetched), display the Tours Component
   return (
     <main>
--- setup/src/Tour.js
+++ setup/src/Tour.js
@@ -13,14 +13,14 @@ const Tour = ({ id, image, info, price, name, removeTour }) => {
         <h4 className="tour-price">${price}</h4>
       </div>
       {/* Ternary that says if readMore is set to true display the entire string, if false only display up to the index of 200*/}
-      <p>{readMore?info:`${info.substring(0,200)}...`}
-      {/* toggles the value of readMore on each click */}
-      <button onClick={()=> setReadMore(!readMore)}>
-        {/* ternary that reads if the button is set to read more then display show less, if not display read more */}
-      {readMore?'show less':'read more'}
-      </button>
+      <p>{readMore ? info : `${info.substring(0, 200)}...`}
+        {/* toggles the value of readMore on each click */}
+        <button onClick={() => setReadMore(!readMore)}>
+          {/* ternary that reads if the button is set to read more then display show less, if not display read more */}
+          {readMore ? 'show less' : 'read more'}
+        </button>
       </p>
-      <button className="delete-btn" onClick={()=> removeTour(id)}>not interested</button>
+      <button className="delete-btn" onClick={() => removeTour(id)}>not interested</button>
     </footer>
   </article>
   );
