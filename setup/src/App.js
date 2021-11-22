3f7864961d74f9487375c3b5285afeb4385ccd08
MaianneThornton <maianne.thornton@gmail.com>
2021-11-22 13:39:05 -0500

    Set up to fetch data from the API as well as set up error handling

--- setup/src/App.js
+++ setup/src/App.js
@@ -5,7 +5,46 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  // Default state will display loading.js
+  const [loading, setLoading] = useState(true);
+  // Default state will display an empty array
+  const [tours, setTours] = useState([]);
+  // if fetching tours always setLoading to true
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      // fetching data from the API
+      const response = await fetch(url);
+      // running the json to give the list of tours
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+      // sets the value of the setTours parameter to the tours variable
+    } catch (error) {
+      // setLoading to false and logs the error
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    // running the fetch tours function to see the array displayed in the console
+    fetchTours();
+  }, [])
+  // if loading is true (fetching data), display loading
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  // if loading is false (data has been fetched), display the Tours Component
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App
