7058733d67688bf7ef8f310c4c62df0830dc2741
MaianneThornton <maianne.thornton@gmail.com>
2021-11-22 13:58:27 -0500

    Displayed the infomation pulled from the API on the page

--- setup/src/Tour.js
+++ setup/src/Tour.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+// destructuring the tour variable information which is pulled from the API
+const Tour = ({ id, image, info, price, name }) => {
+  // displays the image, price, and name, info and button on the page with styling
+  return (<article className="single-tour">
+    <img src={image} alt={name} />
+    <footer>
+      <div className="tour-info">
+        <h4>{name}</h4>
+        <h4 className="tour-price">${price}</h4>
+      </div>
+      <p>{info}</p>
+      <button className="delete-btn">not interested</button>
+    </footer>
+  </article>
+  );
 };
 
 export default Tour;
